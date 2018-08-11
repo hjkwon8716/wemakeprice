e7839e9f68157a123cbe6ddb2cb6226c8336ed3b
권혁준 <hjkwon@nextree.co.kr>
2018-08-11 10:55:46 +0900

    상속 클래스 수정

--- wmp-util/wmp-util-service/src/main/java/joon/wmp/util/UtilApplication.java
+++ wmp-util/wmp-util-service/src/main/java/joon/wmp/util/UtilApplication.java
@@ -3,7 +3,7 @@ package joon.wmp.util;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class UtilApplication extends SpringBootServletInitializer {
